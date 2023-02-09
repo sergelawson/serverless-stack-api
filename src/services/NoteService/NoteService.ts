d33283fab451d11effe15ef2402eea8af77cce8d
Serge Lawson <iamsergelawson@gmail.com>
2023-02-09 18:04:26 +0000

    note service add fetch update delete

--- src/services/NoteService/NoteService.ts
+++ src/services/NoteService/NoteService.ts
@@ -1,20 +1,15 @@
 import Note from "../../models/Note";
 import {
-  PutItemCommand,
-  UpdateItemCommand,
-  ScanCommand,
-  GetItemCommand,
-  DeleteItemCommand,
-} from "@aws-sdk/client-dynamodb";
-
-import {
-  PutCommand,
+  UpdateCommandInput,
+  GetCommandInput,
+  DeleteCommandInput,
+  QueryCommandInput,
   PutCommandInput,
   DynamoDBDocument,
 } from "@aws-sdk/lib-dynamodb";
 
 class NoteService {
-  private TableName = process.env.noteTable;
+  private readonly TableName = process.env.noteTable;
 
   constructor(private dynamodbDoc: DynamoDBDocument) {}
 
@@ -24,9 +19,70 @@ class NoteService {
       Item: note,
     };
 
-    await this.dynamodbDoc.send(new PutCommand(input));
+    await this.dynamodbDoc.put(input);
 
     return note;
+  }
+
+  async getAllNote(userId: string): Promise<Note[]> {
+    const input: QueryCommandInput = {
+      TableName: this.TableName,
+      KeyConditionExpression: "userId = :userId",
+      ExpressionAttributeValues: {
+        ":userId": userId,
+      },
+    };
+
+    const listNotes = await this.dynamodbDoc.query(input);
+
+    return listNotes.Items as Note[];
+  }
+
+  async getNote(userId: string, noteId: string): Promise<Note> {
+    const input: GetCommandInput = {
+      TableName: this.TableName,
+      Key: {
+        userId: userId,
+        noteId: noteId,
+      },
+    };
+
+    const note = await this.dynamodbDoc.get(input);
+
+    return note.Item as Note;
+  }
+
+  async updateNote(note: Note): Promise<any> {
+    const input: UpdateCommandInput = {
+      TableName: this.TableName,
+      Key: {
+        userId: note.userId,
+        noteId: note.noteId,
+      },
+      UpdateExpression: "SET content = :content, attachment = :attachment",
+      ExpressionAttributeValues: {
+        ":content": note.content,
+        ":attachment": note.attachment,
+      },
+      ReturnValues: "ALL_NEW",
+    };
+    await this.dynamodbDoc.update(input);
+
+    return { status: true };
+  }
+
+  async deleteNote(userId: string, noteId: string): Promise<any> {
+    const input: DeleteCommandInput = {
+      TableName: this.TableName,
+      Key: {
+        userId: userId,
+        noteId: noteId,
+      },
+    };
+
+    await this.dynamodbDoc.delete(input);
+
+    return { status: true };
   }
 }
 
